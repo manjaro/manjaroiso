b03940ff42e6c17281e435cc177a76f3eae9b694
Philip Müller <philm@manjarolinux.org>
2012-04-05 16:25:35 +0200

    buildiso: * add make_pkgs_image * update config-files check configs: * update xfce

--- src/build.sh
+++ src/build.sh
@@ -37,12 +37,19 @@ if [ -z "${arch}" ] ; then
 fi
 
 
-#if [ ! -e overlay-pkgs.${arch} ] ; then
-#    echo " "
-#    error "the config file overlay-pkgs.${arch} is missing, exiting..."
-#    echo " "
-#    exit
-#fi
+if [ ! -e Packages ] ; then
+    echo " "
+    error "the config file Packages is missing, exiting..."
+    echo " "
+    exit
+fi
+
+if [ ! -e Packages-Xorg ] ; then
+    echo " "
+    error "the config file Packages-Xorg is missing, exiting..."
+    echo " "
+    exit
+fi
 
 set -e -u
 
@@ -56,9 +63,17 @@ elif [ "${arch}" == "x86_64" ]; then
 fi
 
 if [ "${arch}" == "i686" ]; then
-	xfce_packages=$(sed "s|#.*||g" Packages-Xfce | sed "s| ||g" | sed "s|>dvd.*||g"  | sed "s|>blacklist.*||g" | sed "s|>x86_64.*||g" | sed "s|>i686||g" | sed ':a;N;$!ba;s/\n/ /g')
+	xorg_packages=$(sed "s|#.*||g" Packages-Xorg | sed "s| ||g" | sed "s|>dvd.*||g"  | sed "s|>blacklist.*||g" | sed "s|>x86_64.*||g" | sed "s|>i686||g" | sed ':a;N;$!ba;s/\n/ /g')
 elif [ "${arch}" == "x86_64" ]; then
-	xfce_packages=$(sed "s|#.*||g" Packages-Xfce | sed "s| ||g" | sed "s|>dvd.*||g"  | sed "s|>blacklist.*||g" | sed "s|>i686.*||g" | sed "s|>x86_64||g" | sed ':a;N;$!ba;s/\n/ /g')
+	xorg_packages=$(sed "s|#.*||g" Packages-Xorg | sed "s| ||g" | sed "s|>dvd.*||g"  | sed "s|>blacklist.*||g" | sed "s|>i686.*||g" | sed "s|>x86_64||g" | sed ':a;N;$!ba;s/\n/ /g')
+fi
+
+if [ -e Packages-Xfce ] ; then
+     if [ "${arch}" == "i686" ]; then
+     	xfce_packages=$(sed "s|#.*||g" Packages-Xfce | sed "s| ||g" | sed "s|>dvd.*||g"  | sed "s|>blacklist.*||g" | sed "s|>x86_64.*||g" | sed "s|>i686||g" | sed ':a;N;$!ba;s/\n/ /g')
+     elif [ "${arch}" == "x86_64" ]; then
+     	xfce_packages=$(sed "s|#.*||g" Packages-Xfce | sed "s| ||g" | sed "s|>dvd.*||g"  | sed "s|>blacklist.*||g" | sed "s|>i686.*||g" | sed "s|>x86_64||g" | sed ':a;N;$!ba;s/\n/ /g')
+     fi
 fi
 
 # Base installation (root-image)
@@ -73,19 +88,36 @@ make_root_image() {
     fi
 }
 
-# XFCE installation (xfce-image)
-make_xfce_image() {
+# Prepare pkgs-image
+make_pkgs_image() {
     if [[ ! -e ${work_dir}/build.${FUNCNAME} ]]; then
-         echo -e -n "$_r >$_W XFCE installation (xfce-image) \n $_n"
-         mkdir -p ${work_dir}/xfce-image/var/lib/pacman
-         cp -r ${work_dir}/root-image/var/lib/pacman/local ${work_dir}/xfce-image/var/lib/pacman
-         mkiso -v -C pacman.conf -a "${arch}" -D "${work_dir}/xfce-image" -p "${xfce_packages}" create "${work_dir}"
-         pacman -Qr "${work_dir}/xfce-image" > "${work_dir}/xfce-image/xfce-image-pkgs.txt"
-         cp -Lr xfce-overlay/* ${work_dir}/xfce-image
-         : > ${work_dir}/build.${FUNCNAME}
-         echo -e "$_g >$_W done $_n"
+        echo -e -n "$_r >$_W Prepare pkgs-image \n $_n"
+        mkdir -p ${work_dir}/pkgs-image/opt/manjaro/pkgs
+        mkdir -p ${work_dir}/pkgs-image/var/lib/pacman
+        cp -r ${work_dir}/root-image/var/lib/pacman/local ${work_dir}/pkgs-image/var/lib/pacman
+        pacman -v --config pacman.conf --arch "${arch}" --root "${work_dir}/pkgs-image" --cache ${work_dir}/pkgs-image/opt/manjaro/pkgs -Syw ${xorg_packages} --noconfirm
+        rm -r ${work_dir}/pkgs-image/var
+        repo-add ${work_dir}/pkgs-image/opt/manjaro/pkgs/manjaro-pkgs.db.tar.gz ${work_dir}/pkgs-image/opt/manjaro/pkgs/*pkg*z
+        : > ${work_dir}/build.${FUNCNAME}
+        echo -e "$_g >$_W done $_n"
     fi
 }
+
+if [ ! -e Packages-Xfce ] ; then
+     # XFCE installation (xfce-image)
+     make_xfce_image() {
+         if [[ ! -e ${work_dir}/build.${FUNCNAME} ]]; then
+              echo -e -n "$_r >$_W XFCE installation (xfce-image) \n $_n"
+              mkdir -p ${work_dir}/xfce-image/var/lib/pacman
+              cp -r ${work_dir}/root-image/var/lib/pacman/local ${work_dir}/xfce-image/var/lib/pacman
+              mkiso -v -C pacman.conf -a "${arch}" -D "${work_dir}/xfce-image" -p "${xfce_packages}" create "${work_dir}"
+              pacman -Qr "${work_dir}/xfce-image" > "${work_dir}/xfce-image/xfce-image-pkgs.txt"
+              cp -Lr xfce-overlay/* ${work_dir}/xfce-image
+              : > ${work_dir}/build.${FUNCNAME}
+              echo -e "$_g >$_W done $_n"
+         fi
+     }
+fi
 
 # Prepare ${install_dir}/boot/
 make_boot() {
@@ -152,16 +184,6 @@ make_overlay() {
     fi
 }
 
-# Prepare overlay-pkgs-image
-make_overlay_pkgs() {
-    if [[ ! -e ${work_dir}/build.${FUNCNAME} ]]; then
-        echo -e -n "$_r >$_W Prepare overlay-pkgs-image \n $_n"
-        overlay-pkgs ${arch} ${work_dir}
-        : > ${work_dir}/build.${FUNCNAME}
-        echo -e "$_g >$_W done $_n"
-    fi
-}
-
 # Process isomounts
 make_isomounts() {
     if [[ ! -e ${work_dir}/build.${FUNCNAME} ]]; then
@@ -197,12 +219,17 @@ else
     high_compression=""
 fi
 
+# install basic
 make_root_image
-make_xfce_image
+make_pkgs_image
+# install DE(s)
+if [ ! -e Packages-Xfce ] ; then
+    make_xfce_image
+fi
+# install common
 make_boot
 make_syslinux
 make_isolinux
 make_overlay
-#make_overlay_pkgs
 make_isomounts
 make_iso
