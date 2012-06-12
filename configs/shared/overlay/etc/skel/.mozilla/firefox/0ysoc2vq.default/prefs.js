bc5006317c4ad4bec2fa9de85bfa643088aa0b1a
guinux <nuxgui@gmail.com>
2012-06-12 10:48:26 +0200

    update skel/.mozilla to firefox13

--- configs/shared/overlay/etc/skel/.mozilla/firefox/0ysoc2vq.default/prefs.js
+++ configs/shared/overlay/etc/skel/.mozilla/firefox/0ysoc2vq.default/prefs.js
@@ -6,30 +6,31 @@
  * the changes will be overwritten when the application exits.
  *
  * To make a manual change to preferences, you can visit the URL about:config
- * For more information, see http://www.mozilla.org/unix/customizing.html#prefs
  */
 
 user_pref("browser.bookmarks.restore_default_bookmarks", false);
 user_pref("browser.cache.disk.capacity", 1048576);
 user_pref("browser.cache.disk.smart_size.first_run", false);
 user_pref("browser.cache.disk.smart_size_cached_value", 302437);
-user_pref("browser.migration.version", 5);
-user_pref("browser.places.smartBookmarksVersion", 2);
+user_pref("browser.migration.version", 6);
+user_pref("browser.places.smartBookmarksVersion", 3);
 user_pref("browser.rights.3.shown", true);
 user_pref("browser.startup.homepage", "http://manjarolinux.org");
-user_pref("browser.startup.homepage_override.buildID", "20110619060346");
-user_pref("browser.startup.homepage_override.mstone", "rv:5.0");
-user_pref("extensions.blocklist.pingCountVersion", 0);
+user_pref("browser.startup.homepage_override.buildID", "20120603173740");
+user_pref("browser.startup.homepage_override.mstone", "13.0");
+user_pref("extensions.blocklist.pingCountVersion", -1);
 user_pref("extensions.bootstrappedAddons", "{}");
-user_pref("extensions.databaseSchema", 3);
-user_pref("extensions.enabledAddons", "langpack-de@firefox.mozilla.org:5.0,{972ce4c6-7e08-4474-a285-3208198ce6fd}:5.0");
-user_pref("extensions.installCache", "[{\"name\":\"app-global\",\"addons\":{\"langpack-de@firefox.mozilla.org\":{\"descriptor\":\"/usr/lib/firefox-5.0/extensions/langpack-de@firefox.mozilla.org.xpi\",\"mtime\":1308478616000},\"{972ce4c6-7e08-4474-a285-3208198ce6fd}\":{\"descriptor\":\"/usr/lib/firefox-5.0/extensions/{972ce4c6-7e08-4474-a285-3208198ce6fd}\",\"mtime\":1262300400000}}}]");
-user_pref("extensions.lastAppVersion", "5.0");
+user_pref("extensions.databaseSchema", 12);
+user_pref("extensions.enabledAddons", "langpack-de@firefox.mozilla.org:13.0,langpack-fr@firefox.mozilla.org:13.0,{972ce4c6-7e08-4474-a285-3208198ce6fd}:13.0");
+user_pref("extensions.installCache", "[{\"name\":\"app-global\",\"addons\":{\"langpack-de@firefox.mozilla.org\":{\"descriptor\":\"/usr/lib/firefox/extensions/langpack-de@firefox.mozilla.org.xpi\",\"mtime\":1338920045000},\"langpack-fr@firefox.mozilla.org\":{\"descriptor\":\"/usr/lib/firefox/extensions/langpack-fr@firefox.mozilla.org.xpi\",\"mtime\":1338920061000},\"{972ce4c6-7e08-4474-a285-3208198ce6fd}\":{\"descriptor\":\"/usr/lib/firefox/extensions/{972ce4c6-7e08-4474-a285-3208198ce6fd}\",\"mtime\":1338746031000}}}]");
+user_pref("extensions.lastAppVersion", "13.0");
+user_pref("extensions.lastPlatformVersion", "13.0");
 user_pref("extensions.pendingOperations", false);
 user_pref("intl.charsetmenu.browser.cache", "ISO-8859-1, UTF-8");
 user_pref("network.cookie.prefsMigrated", true);
-user_pref("places.history.expiration.transient_current_max_pages", 21002);
+user_pref("places.history.expiration.transient_current_max_pages", 18852);
 user_pref("privacy.sanitize.migrateFx3Prefs", true);
-user_pref("urlclassifier.keyupdatetime.https://sb-ssl.google.com/safebrowsing/newkey", 1312541625);
+user_pref("toolkit.startup.last_success", 1339497499);
+user_pref("urlclassifier.keyupdatetime.https://sb-ssl.google.com/safebrowsing/newkey", 1342088488);
 user_pref("xpinstall.whitelist.add", "");
 user_pref("xpinstall.whitelist.add.36", "");
