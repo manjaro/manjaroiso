31a9f573ad9a6b94c2042f8235128e856ab2a238
Philip <philm@manjaro.org>
2014-01-12 11:17:32 +0100

    KDE: update turublence autostart

--- configs/kde/kde-overlay/etc/skel/.config/autostart/firstboot.py
+++
@@ -1,8 +0,0 @@
-
-#!/bin/sh
-if [ -f ~/.config/autostart/turbulence.desktop ]; then
-    #Removes the autostart file, so it won't start again at first boot.
-    rm ~/.config/autostart/turbulence.desktop 
-fi
-#end / for turbulence
- 
